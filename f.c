f5c796a12ca0e8202aaa1a4e6b3310c18b94a05f
kskrajny <j.skrajny@student.uw.edu.pl>
2020-04-03 13:58:57 +0200

    FINISH

--- f.c
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-void pix(uint32_t *ppi, uint64_t *pidx, uint64_t max){
-	*ppi = max;
-	*pidx = max;
-	return;
-}
--- pix.asm
+++ pix.asm
@@ -1,21 +1,21 @@
 section .text
-%macro addFract 0       ;dodaje cyfry rozwiniecia ulamka
-    mov r9, 8           ;tworzymy mianownik
-    imul r9, r12
-    add r9, r8          ;mianownik: r9 <- 8r9+r8
-    mov r8, r11         ;iterator jako r8
-    mov rax, 1
-%%loop:                 ;w petli tworzymy 16^(8r11)/q mod(2^64)
-    mov rdx, 0
-    cmp r8, 1
-    jl %%minus
+%macro addFract 0       ;dodaje wartosc opisana nizej do rbx
+    mov r9, 8           ;tworzymy mianownik z formuly BBP (8n+i)
+    imul r9, r12        ;n=r12, i=r8
+    add r9, r8          ;mianownik jest w r9
+    mov r8, r11         ;zmiana roli r8 na iterator
+    mov rax, 1          
+%%loop:                 ;miedzy %%loop a %%last tworzymy wartosc
+    mov rdx, 0          ;32 wazniejsze bity to cyfry na pozycjach,
+    cmp r8, 1           ;   ktore chcemy aktulanie policzyc
+    jl %%minus          ;kolejne 16 biów poprawia precyzje
     je %%endLoop
     shl rax, 32
     div r9
     dec r8
     mov rax, rdx
     jmp %%loop
-%%endLoop:
+%%endLoop:                
     shl rax, cl
     div r9
     mov rax, rdx
@@ -27,73 +27,69 @@ section .text
     shl rax, cl
     shl rax, 16
     div r9
-%%last:
+%%last:                 
     mov rdx, 0
     imul r10                        
-    add rbx, rax        ;dodajemy wynik mod(2^64) 
+    add rbx, rax        ;rax = (r10\(8r12+r8)*2^(32*(r11)+16))%2^48
 %endmacro
 
 global pix
 extern pixtime
 pix:
-    push rdi
+    push rdi            ;pushuje na stos aby uniknac problemow
     push rdx
     push rcx
     push rbp
     push rsi
 
-    rdtsc
-    mov rdi, rdx
+    rdtsc               
+    mov rdi, rdx        
     shl rdi, 32
-    add rdi, rax
+    add rdi, rax        ;do rdi przenosze caly wynik rdtsc
     call pixtime
-    
+
     pop rsi
     pop rbp
     pop rcx
     pop rdx
     pop rdi
-    push r14
+    push r14            ;te rejestry bede chcial zmieniac
     push r13
     push r12
     push rbx
-    mov r14, rdx
+    mov r14, rdx        ;zapisuje argument max funkcji pix
                             ;rdi - *ppi - wskaznik na tablic
                             ;rdx - max
                             ;rsi - *pidx - wskaznik na iterator
 _takeNext:
-    mov r12, 1
-    lock xadd [rsi], r12
+    mov r12, 1          ;zaraz pobiire wartosc spod wskaznika rsi
+    lock xadd [rsi], r12;to wywolanie jest atomowe
     inc r12
-    cmp r12, r14
-    jg _exit
-    mov r13, r12
+    cmp r12, r14        ;porownuje z max,
+    jg _exit    
+    mov r13, r12        ;zapisuje do r13, bo ...
     dec r13
-    imul r12, 8
-    inc r12
-                            ;r12 iterator
-                            ;r9 przez to dzielimy w addFract
-                            ;r8 pomaga stworzyc mianownik
-                            ;r10 bedzie licznikiem ulamkow    
-    mov cl, 0               ;to mnozyc razy 1/q
-    mov r11, 0              ;ile razy mnozyc 1/q przez 2^32
-    mov rbx, 0              ;tu bedzie wynik
+    imul r12, 8         ;... r12 pos;uzy jako iterator
+    inc r12  
+    mov cl, 0           ;2^cl, przez tyle beds mnozone ułamki
+    mov r11, 0          ;ile razy mnozyc ulamek przez 2^32
+    mov rbx, 0          ;tu bedzie wynik
 _sumLoop:
     
-    cmp cl, 32
-    jne _skip
-    mov cl, 0
-    inc r11
+    cmp cl, 32          ;jesli cl to 32 
+    jne _skip           
+    mov cl, 0           ;wyzerujemy cl oraz zwiekszymy r11
+    inc r11             
     
 _skip: 
-    cmp r12, 0
-    je _endSum
+    cmp r12, 0          ;r12 konczy zliczanie sumy z BBP
+    je _endSum          
     dec r12
 
-    mov r10, 4
-    mov r8, 1
-    addFract
-
+    mov r10, 4           ;r10 - licznik ulamkow z BBP {4,-2,-1}
+    mov r8, 1            ;r8 pomoze zapisac mianownik z BBP do r9
+    addFract             ;(r10\(8r12+r8)*2^(32*(r11)+16))%2^48
+                         ;addFract powyzsza wartosc dodaje do rbx
     mov r10, -2
     mov r8, 4
     addFract
@@ -112,15 +108,15 @@ _endSum:
 
     xor rax, rax
     mov rax, rbx
-    shl rax, 16
-    shr rax, 32
-    shl r13, 2
-    add r13, rdi
-    mov [r13], eax
+    shl rax, 16         ;pozbywamy się zbędnych cyfr
+    shr rax, 32         ;to tez
+    shl r13, 2          ;szukamy adresu w tablicy ppi,
+    add r13, rdi        ;   aby wisac wynik
+    mov [r13], eax      
 
     jmp _takeNext
 _exit:
-      push rdi
+    push rdi            ;pushuje na stos aby uniknac problemow
     push rdx
     push rcx
     push rbp
@@ -132,7 +128,7 @@ _exit:
     add rdi, rax
     call pixtime
     
-    pop rsi
+    pop rsi             ;zwracam do rejestrow to co zabralem
     pop rbp
     pop rcx
     pop rdx
